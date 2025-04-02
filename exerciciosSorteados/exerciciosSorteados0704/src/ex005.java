8caeabc4ba63946d09d21299820bb28eaf06f450
Ana Carolina A Pereira <anacarolinaaguiarp@gmail.com>
2025-04-02 17:30:25 -0300

    exerciciosSorteaveis

---
+++ exerciciosSorteados/exerciciosSorteados0704/src/ex005.java
@@ -0,0 +1,11 @@
+/* Desenvolva uma classe Pessoa possa, opcionalmente, ser instanciada com o CPF ou a idade da Pessoa sendo informada por parâmetro. Desenvolva o código da classe contendo o método main para testar o uso da classe Pessoa. Envie o código Java como resposta. . . . . . . . .
+5.1. Get e Set
+Atualize a suas classes que acessam e modificam atributos de uma Pessoa para utilizar os get e set. Indique o que foi alterado nos comentários do arquivo Java.
+5.2. CPF
+Considere que na classe Pessoa tem um CPF como atributo. Como garantir que alguma pessoa não seja criada caso tenha CPF inválido e tampouco seja criado um objeto Pessoa sem informar o CPF? Implemente a solução e o teste no método main. */
+
+public class ex005 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ exerciciosSorteados/exerciciosSorteados0704/src/ex006.java
@@ -0,0 +1,7 @@
+/* Utilizando como base a classe Pessoa implementada nas atividades anteriores, desenvolva um sistema de cursos, onde um Pessoa pode ser um Aluno ou um Instrutor. O sistema deve informar quais Alunos estão matriculados em qual curso e qual o instrutor de cada curso. Desenvolva as classes necessárias e a classe com o método main para testar a aplicação. */
+
+public class ex006 {
+    public static void main(String[] args) {
+        
+    }
+}
