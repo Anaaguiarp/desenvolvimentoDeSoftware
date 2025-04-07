5856b0ccb1c9718bbedeb99cabbd1b1c5de98c4b
Ana Carolina A Pereira <anacarolinaaguiarp@gmail.com>
2025-04-06 23:42:53 -0300

    Delete ex006 directory

--- ex006/src/Aluno.java
+++
@@ -1,5 +0,0 @@
-public class Aluno extends Pessoa {
-    public Aluno(String nome, String cpf) {
-        super(nome, cpf);
-    }
-}
--- ex006/src/Instrutor.java
+++
@@ -1,5 +0,0 @@
-public class Instrutor extends Pessoa {
-    public Instrutor(String nome, String cpf) {
-        super(nome, cpf);
-    }
-}
